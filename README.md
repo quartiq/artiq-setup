d81042725a7af95e6aa27ba4a9b8df4619d588a7
Robert Jordens <jordens@gmail.com>
2018-03-08 15:22:57 +0000

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# Controller
+
+* Kasli/v1.1
+* Serial: #24
+* EUI-48: 54-10-ec-aa-da-a7
+* MAC: 54:10:ec:aa:da:a7
+* IP address: 10.0.16.30
+* Gateware, bootloader, firmware: artiq-4.0.dev-862f2b42
+
+## EEM0
+
+* DIO_BNC/v1.2
+* EUI-48: 54-10-ec-a9-fe-17
+* Channels 0-3: input
+* Channels 4-7: output
+* Termination: none
+
+## EEM1
+
+* DIO_BNC/v1.2
+* EUI-48: 54-10-ec-a9-67-bf
+* Channels 0-3: output
+* Channels 4-7: output
+* Termination: none
+
+## EEM4, EEM5
+
+* Urukul-AD9910/v1.1
+* EUI-48: 54-10-ec-35-57-d8, 54-10-ec-35-75-23
+* 100 MHz XO on clk_sel=0
+* using EEM1 for SW[0:3]
+
+## EEM6
+
+* Urukul-AD9912/v1.1
+* EUI-48: 54-10-ec-35-90-47
+* 100 MHz XO on clk_sel=0
---
+++ repository/startup.py
@@ -0,0 +1,39 @@
+from artiq.experiment import *
+
+
+class Startup(EnvExperiment):
+    def build(self):
+        self.setattr_device("core")
+
+        for u in range(2):
+            self.setattr_device("urukul{}_cpld".format(u))
+            for ch in range(4):
+                self.setattr_device("urukul{}_ch{}".format(u, ch))
+
+        self.setattr_device("led0")
+        self.setattr_device("cool0")
+
+    @kernel
+    def run(self):
+        self.core.reset()
+
+        delay(20*ms)
+        self.urukul0_cpld.init()
+        delay(20*ms)
+        self.urukul0_ch0.init()
+        self.urukul0_ch1.init()
+        self.urukul0_ch2.init()
+        self.urukul0_ch3.init()
+        delay(20*ms)
+        self.urukul1_cpld.init()
+        delay(20*ms)
+        self.urukul1_ch0.init()
+        self.urukul1_ch1.init()
+        self.urukul1_ch2.init()
+        self.urukul1_ch3.init()
+
+        delay(10*ms)
+        for i in range(3):
+            self.led0.pulse(.1*s)
+            self.led0.sync()
+            delay(.1*s)
